9edb6bac4a8f51d49458f92c742014a275b6dab6
ktubot <121492934+ktubot@users.noreply.github.com>
2024-10-17 13:09:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,11 @@ const examsDetails = {
                 {"code": "EST102", "date": "June 18, 2019 9:30:00", "name": "Programming in C", "id":6},
             ],
             "2024": [
-                { "code": "MAT101", "date": "October 26, 2024 9:30:00", "name": "Mathematics", "id": 1 },
-                {"code": "PHT100", "date": "May 31, 2024 9:30:00", "name": "Physics For Information Science", "id":2},
-                {"code": "EST100", "date": "June 5, 2024 9:30:00", "name": "Engineering Graphics ", "id":3},
-                {"code": "EST120", "date": "June 10, 2024 9:30:00", "name": "Intro To Electrical & Electronics", "id":4},
-                {"code": "HUN102", "date": "July 13, 2024 9:30:00", "name": "Algorithmic Thinking With Python", "id":5},
-                {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
+{ "code": "GAMAT101", "date": "December 31, 2024 9:30:00", "name": "MATHS", "id": 1 },
+{ "code": "GAPHT121/GXCYT122", "date": "January 3, 2025 9:30:00", "name": "PHY/CHEM", "id": 2 },
+{ "code": "GMEST103", "date": "January 6, 2025 9:30:00", "name": "GRAPHICS", "id": 3 },
+{ "code": "GXEST104", "date": "January 8, 2025 9:30:00", "name": "IEEE", "id": 4 },
+{ "code": "UCEST105", "date": "January 10, 2025 9:30:00", "name": "PYTHON", "id": 5 }
             ]
         },
         "EC": {

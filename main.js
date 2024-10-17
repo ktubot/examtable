2e07c8a49daaca29b4a23c7ef5c66f56e0ad552e
ktubot <121492934+ktubot@users.noreply.github.com>
2024-10-17 16:25:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -103,7 +103,8 @@ const examsDetails = {
 { "code": "UCEST105", "date": "January 10, 2025 1:30:00", "name": "Python", "id": 5 }
             ]
         },
-    };
+    }
+};
     "S3": {
         "CS": {
             "2019": [
